6c04480656d2a2dca583edbe1617a1487d8ba57e
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2011-03-06 08:44:25 +0900

    adding akihabara lib. its javascript game engine.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById('island');
-
 var ctx = canvas.getContext('2d');
-ctx.strokeRect(2, 2, canvas.width-4, canvas.height-4);
+
+gbox.blitRect(ctx, {x:1, y:2, w:100, h:200, color: '#0080FF'});
+gbox.blitRect(ctx, {x:1, y:2, w:100, h:200, color: '#0080FF'});
